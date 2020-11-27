d6c520d953923dfa9cfc610cb07320c51f1eeee9
root <root@DESKTOP-5PBM1PP.localdomain>
2020-11-27 21:45:39 +0900

    v3 struct

--- go_struct_01.go
+++ go_struct_01.go
@@ -2,36 +2,29 @@ package main
 
 import "fmt"
 
-type subscriber struct {
-	name   string
-	rate   int
-	active bool
+type part struct {
+	description string
+	count       int
+}
+
+func showInfo(p part) {
+	fmt.Println("Description : ", p.description)
+	fmt.Println("Count : ", p.count)
+}
+
+func minimumOrder(description string) part {
+	var p part
+	p.description = description
+	p.count = 1000
+	return p
 }
 
 func main() {
-	var s1 subscriber
+	p := minimumOrder("Hex bolts")
+	fmt.Println(p.description, p.count)
 
-	fmt.Printf("%#v\n", s1)
-
-	s1.name = "kim"
-	s1.rate = 5000
-	s1.active = false
-
-	fmt.Printf("%s\n", s1.name)
-	fmt.Println(s1.rate)
-	fmt.Println(s1.active)
-
-	/*
-		subscriber := map[string]float64{}
-		subscriber["name"] = "kim"
-		subscriber["rate"] = 5000
-		subscriber["active"] = false
-		fmt.Println("test")
-
-		subscriber := []string{}
-		subscriber = append(subscriber,"kim")
-		subscriber = append(subscriber,5000)
-		subscriber = append(subscriber,false)
-		fmt.Println("test")
-	*/
+	//var bolts part
+	//bolts.description = "Hex bolts"
+	//bolts.count = 100
+	//showInfo(bolts)
 }
